2d03888f8024605eec72a19fbb446abeee802786
ccjmk <iguastalli@outlook.com>
2013-05-29 22:25:41 -0300

    Attack comment

--- BatallaPokemon/src/ia/battle/entities/Attack.java
+++ BatallaPokemon/src/ia/battle/entities/Attack.java
@@ -1,7 +1,8 @@
 package ia.battle.entities;
 
 public final class Attack extends Action {
-
+	//attack es compartido por todos los warriors
+	//porque es clase final y no podemos extenderla por warrior
 	public final void attack() {
 	}
 }
